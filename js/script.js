5a09fa5d579db8a382103bdcd2eea17df78df203
lvivduncan <lvivduncan@gmail.com>
2021-12-03 13:12:58 +0200

    #nav finish

--- js/script.js
+++ js/script.js
@@ -13,26 +13,76 @@
     const nav = document.getElementById('nav')
     nav.append(navButtonClose)
 
-    document.getElementById('search-button').addEventListener('click', showMenu)
-
-    document.getElementById('nav-button').addEventListener('click', showMenu)
-
-    navWrapper.addEventListener('click', hideMenu)
-
-    navButtonClose.addEventListener('click', hideMenu)
-
-    function showMenu(){
+    // open
+    document.getElementById('search-button').addEventListener('click', () => {
 
         nav.classList.add('active')
         navWrapper.classList.add('active')
         body.classList.add('fixed')
-    }
+        document.querySelector('#nav input[type=search]').classList.add('active')      
+    })
 
-    function hideMenu(){
+    document.getElementById('nav-button').addEventListener('click', () => {
+
+        nav.classList.add('active')
+        navWrapper.classList.add('active')
+        body.classList.add('fixed')        
+    })
+
+    document.getElementById('nav-menu').addEventListener('click', event => {
+
+        event.preventDefault()
+
+        nav.classList.add('active')
+        navWrapper.classList.add('active')
+
+        document.body.scrollTop = document.documentElement.scrollTop = 0 
+    })
+
+    document.getElementById('nav-search').addEventListener('click', event => {
+
+        event.preventDefault()  
+
+        nav.classList.add('active')
+        navWrapper.classList.add('active')
+
+        document.querySelector('#nav input[type=search]').classList.add('active')
+
+        document.body.scrollTop = document.documentElement.scrollTop = 500
+    })
+
+    // close
+    navWrapper.addEventListener('click', () => {
 
         nav.classList.remove('active')
         navWrapper.classList.remove('active')
         body.classList.remove('fixed')
-    }
 
+        document.querySelector('#nav input[type=search]').classList.remove('active')
+
+        document.body.scrollTop = document.documentElement.scrollTop = 0 
+    })
+
+    navButtonClose.addEventListener('click', () => {
+
+        nav.classList.remove('active')
+        navWrapper.classList.remove('active')
+        body.classList.remove('fixed')
+
+        document.querySelector('#nav input[type=search]').classList.remove('active')
+
+        document.body.scrollTop = document.documentElement.scrollTop = 0
+    })
+
+    document.addEventListener('keydown', event => {
+
+        if(event.key === 'Escape' || event.code === 'Escape'){
+
+            nav.classList.remove('active')
+            navWrapper.classList.remove('active')
+            body.classList.remove('fixed')
+
+            document.querySelector('#nav input[type=search]').classList.remove('active')
+        }
+    })
 }
